5b9e3730d14b4366dfd129f12ec8f5b5b3263480
baixiaojian <15101661380@163.com>
2018-06-05 13:40:37 +0800

    test: touch off webhook

--- auto_build.sh
+++ auto_build.sh
@@ -3,7 +3,8 @@
 git reset --hard origin/master
 git clean -f
 git pull origin master
+npm stop
 cnpm install
 # npm run test
-EGG_SERVER_ENV=prod  npm start
+EGG_SERVER_ENV=test  npm start
 # npm run start
