8085baa9ce316515924d6eb45dc22b2a71a4e824
baixiaojian <15101661380@163.com>
2018-06-05 15:32:32 +0800

    test: web hook

--- auto_build.sh
+++ auto_build.sh
@@ -1,10 +1,8 @@
-#! /bin/bash
+# ! /bin/bash
 
 git reset --hard origin/master
 git clean -f
 git pull origin master
-npm stop
 cnpm install
-# npm run test
-EGG_SERVER_ENV=test  npm start
-# npm run start
+npm stop
+EGG_SERVER_ENV=test npm start
--- webhook.js
+++ webhook.js
@@ -1,7 +1,21 @@
 'use strict';
 const http = require('http');
 const createHandler = require('github-webhook-handler');
-const handler = createHandler({ path: '/webhook', secret: 'truechain_xiaojian' });
+const handler = createHandler({
+  path: '/webhook',
+  secret: 'truechain_xiaojian',
+});
+const { spawn } = require('child_process');
+
+const runCommand = (cmd, args, callback) => {
+  const child = spawn(cmd, args);
+  let response = '';
+  child.stdout.on('data', buffer => {
+    response += buffer.toString();
+  });
+  child.stdout.on('end', () => callback(response));
+};
+
 
 http.createServer(function(req, res) {
   handler(req, res, function(err) {
@@ -15,10 +29,12 @@ handler.on('error', function(err) {
 });
 
 handler.on('push', function(event) {
-  // sadasds
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
+  runCommand('sh', [ './auto_build.sh' ], txt => {
+    console.log(txt);
+  });
 });
 
 handler.on('issues', function(event) {
