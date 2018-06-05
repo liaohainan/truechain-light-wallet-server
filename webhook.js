2b4063a04c7bc3c551c94ca5c7a0f5c524d8360f
baixiaojian <15101661380@163.com>
2018-06-05 14:10:12 +0800

    test: touch off webhook

--- webhook.js
+++ webhook.js
@@ -15,6 +15,7 @@ handler.on('error', function(err) {
 });
 
 handler.on('push', function(event) {
+  // sadasds
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
