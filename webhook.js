f3d12741d82b6ef3a8f532b33172a2599c94739d
baixiaojian <15101661380@163.com>
2018-06-05 14:06:21 +0800

    feat: github-webhook-handler

--- webhook.js
+++ webhook.js
@@ -1,33 +1,29 @@
 'use strict';
 const http = require('http');
-const createHandler = require('coding-webhook-handler');
-// as
-const handler = createHandler({
-  path: '/webhook',
-  token: 'truechain_xiaojian', // 在 coding 上面可以填写一个 token
-});
+const createHandler = require('github-webhook-handler');
+const handler = createHandler({ path: '/webhook', secret: 'truechain_xiaojian' });
 
-// const rumCommand = (cmd, args, callback) => {
-//   const child = spawn(cmd, args);
-//   let response = '';
-//   child.stdout.on('data', buffer => response += buffer.toString());
-//   child.stdout.on('end', () => callback(response));
-// };
-
-http.createServer((req, res) => {
+http.createServer(function(req, res) {
   handler(req, res, function(err) {
     res.statusCode = 404;
     res.end('no such location');
   });
 }).listen(7777);
 
-handler.on('error', err => {
+handler.on('error', function(err) {
   console.error('Error:', err.message);
 });
 
-handler.on('push', event => {
-  console.log('成功了');
-  // rumCommand('sh', [ './auto_build.sh' ], txt => {
-  //   console.log(txt);
-  // });
+handler.on('push', function(event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+});
+
+handler.on('issues', function(event) {
+  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title);
 });
